5ae2c1214d3c7286f1640a1d5b7181c56ff4378d
Gundars <Gundars2.Pelns@live.uwe.ac.uk>
2017-11-20 13:04:28 +0000

    Gaia

---
+++ GaiaIndentAnimation/Assets/Camera/CameraMove.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public Vector3 center;
+    public Transform objCenter; 
+
+    Vector3 vel = Vector3.zero;
+    float flySpeed;
+
+    public bool reverse;
+    public bool stop;
+    public bool up;
+
+
+    void Start () {
+        flySpeed = 0.5f;
+        reverse = false;
+        stop = false;
+        up = false;
+	}
+	
+	void Update () {
+        //vel = vel + new Vector3(, , ); Y Z
+        if (!stop)
+        {
+            if (!reverse)
+            {
+                vel.z = vel.z - 0.0005f * (center.z - transform.position.z);
+                if (transform.position.z < -50)
+                {
+                    reverse = true;
+                    up = true;
+                }
+            }
+            if (reverse)
+            {
+                vel.z = vel.z + 0.00025f * (center.z - transform.position.z);
+                if (transform.position.z > -40)
+                {
+                    stop = true;
+                }
+            }
+        }
+
+        if (stop)
+        {
+            if (transform.position.z > -10)
+            {
+
+            }
+            else
+            {
+                vel.z = vel.z - 0.001f * (center.z - transform.position.z);
+            }
+        }
+
+        if (up)
+        {
+            transform.position = transform.position + new Vector3(0f,flySpeed,0f);
+        }
+
+        transform.position = transform.position + vel;
+        transform.LookAt(objCenter);
+	}
+}
---
+++ GaiaIndentAnimation/Assets/MoveHouseUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHouseUp : MonoBehaviour {
+
+    float speed;
+    float current;
+    public float end;
+
+	// Use this for initialization
+	void Start () {
+        current = 0;
+        end = 1500;
+        speed = Random.Range(0.2f, 0.3f);
+        transform.localScale = new Vector3(100f, 0f, 100f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (current < end)
+        {
+            //transform.Translate(Vector3.up * speed * Time.deltaTime);
+            transform.localScale += new Vector3(0, speed);
+            current += 5;
+        }
+        
+	}
+}
